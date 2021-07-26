ad937f2c8cc186ef200ad6c35051720d46ee9d94
Lupo Solitario <duyluong1994@gmail.com>
2021-07-26 11:38:29 +0700

    enable authority

---
+++ contracts/cardrewards/addcode.sh
@@ -0,0 +1,2 @@
+CLEOS="cleos -u https://wax.greymass.com"
+$CLEOS set account permission colosseumfee active --add-code
--- contracts/cardrewards/src/cardrewards.cpp
+++ contracts/cardrewards/src/cardrewards.cpp
@@ -1,64 +1,73 @@
 #include <cardrewards.hpp>
 
-ACTION cardrewards::claim(name account) {
+ACTION cardrewards::claim(name account)
+{
     // Authority check
     check(has_auth(account), "Insufficient authority.");
     claim_m(account);
 }
 
-ACTION cardrewards::claimrholder(name claimer, name rholder) {
+ACTION cardrewards::claimrholder(name claimer, name rholder)
+{
     // Authority check
     check(has_auth(claimer), "Insufficient authority.");
     check_has_auth(claimer, "Claimer is not authorized.");
     claimrholder_m(claimer, rholder);
 }
 
-ACTION cardrewards::claimref(name claimer, name referral) {
+ACTION cardrewards::claimref(name claimer, name referral)
+{
     // Authority check
     check(has_auth(claimer), "Insufficient authority.");
     check_has_auth(claimer, "Claimer is not authorized.");
     claimref_m(claimer, referral);
 }
 
-ACTION cardrewards::regcol(name collection_name) {
+ACTION cardrewards::regcol(name collection_name)
+{
     // Authority check
-//    check(has_auth(_self), "Insufficient authority.");
+    check(has_auth(_self), "Insufficient authority.");
     regcol_m(collection_name);
 }
 
-ACTION cardrewards::delcol(name collection_name) {
+ACTION cardrewards::delcol(name collection_name)
+{
     // Authority check
-//    check(has_auth(_self), "Insufficient authority.");
+    check(has_auth(_self), "Insufficient authority.");
     delcol_m(collection_name);
 }
 
-ACTION cardrewards::regauth(name authorizer) {
+ACTION cardrewards::regauth(name authorizer)
+{
     // Authority check
-//    check(has_auth(_self), "Insufficient authority.");
+    check(has_auth(_self), "Insufficient authority.");
     regauth_m(authorizer);
 }
 
-ACTION cardrewards::delauth(name authorizer) {
+ACTION cardrewards::delauth(name authorizer)
+{
     // Authority check
-//    check(has_auth(_self), "Insufficient authority.");
+    check(has_auth(_self), "Insufficient authority.");
     delauth_m(authorizer);
 }
 
-ACTION cardrewards::addmap(name player_name, name rholder, name referral) {
+ACTION cardrewards::addmap(name player_name, name rholder, name referral)
+{
     // Authority check
-//    check(has_auth(_self), "Insufficient authority.");
+    check(has_auth(_self), "Insufficient authority.");
     addmap_m(player_name, rholder, referral);
 }
 
-ACTION cardrewards::delmap(name player_name) {
+ACTION cardrewards::delmap(name player_name)
+{
     // Authority check
-//    check(has_auth(_self), "Insufficient authority.");
+    check(has_auth(_self), "Insufficient authority.");
     delmap_m(player_name);
 }
 
-ACTION cardrewards::updatemap(name player_name, name rholder, name referral) {
+ACTION cardrewards::updatemap(name player_name, name rholder, name referral)
+{
     // Authority check
-//    check(has_auth(_self), "Insufficient authority.");
+    check(has_auth(_self), "Insufficient authority.");
     updatemap_m(player_name, rholder, referral);
 }
-
